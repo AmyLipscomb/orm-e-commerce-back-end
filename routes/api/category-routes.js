a9ad1f8cd726d066512b880d5e63d109de7a1730
Amy Lipscomb <amylipscomb@amys-mbp.myfiosgateway.com>
2022-12-06 18:54:15 -0500

    added code to the product-routes.js file and updated the code for the PUT route in the category-routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,39 +5,39 @@ const { Category, Product, Tag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all categories
-    try{
-      const categoriesData = await Category.findAll();
-      res.status(200).json(categoriesData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  try {
+    const categoriesData = await Category.findAll();
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products - HELP!
-    // include: [{model: Category, through: Product}]
+  // include: [{model: Category, through: Product}]
 });
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  try{
+  try {
     const categoriesData = await Category.findbyPk(req.params.id, {
       //NOT SURE ABOUT THE CODE THAT GOES HERE 
-        //include: 
+      //include: 
     });
-    if (!categoriesData){
-      res.status(404).json({ message: 'No category found with this id!'});
+    if (!categoriesData) {
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
     res.status(200).json(categoriesData);
-    } catch (err){
-      res.status(500).json(err);
-    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
-    //NEED HELP HERE
+  //NEED HELP HERE
   // be sure to include its associated Products 
 });
 
 router.post('/', async (req, res) => {
   // create a new category
-  try{
+  try {
     const categoriesData = await Category.create(req.body);
     res.status(200).json(categoriesData);
   } catch (err) {
@@ -47,27 +47,33 @@ router.post('/', async (req, res) => {
 
 // NOT SURE ABOUT THIS ONE 
 router.put('/:id', async (req, res) => {
-  // try{
-  //   const categoriesData = await Category
-  // }
-  // // update a category by its `id` value
+  try{
+      await Category.update(req.body, {
+        // update a category by its `id` value
+        where: {
+          id: req.params.id,
+        },
+      })
+    } catch (err) {
+      res.status(400).json(err);
+    }
 });
 
 router.delete('/:id', async (req, res) => {
   try {
     const categoriesData = await Category.destroy({
-      
+
       // delete a category by its `id` value
       where: {
         id: req.params.id
       }
     });
-    if (!categoriesData){
-      res.status(404).json({ message: 'No category found with this id!'});
+    if (!categoriesData) {
+      res.status(404).json({ message: 'No category found with this id!' });
       return;
     };
     res.status(200).json(categoriesData);
-  } catch (err){
+  } catch (err) {
     res.status(500).json(err);
   }
 });
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,17 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try{
+    const productData = await Product.findAll();
+    res.status(200).json(productData);
+  } catch (err){
+    res.status(500).json(err);
+  }
+  //NEED HELP WITH THIS!
+    // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try{
+    const productData = await Product.findbyPk(req.params.id,{
+       //NOT SURE ABOUT THE CODE THAT GOES HERE 
+      //include: 
+    });
+    if (!productData){
+      res.status(404).json({ message: 'No product found with this id!'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+    //NEED HELP HERE
+      // be sure to include its associated Category and Tag data
 });
 
+// HAVEN'T DONE ANYTHING TO THIS CODE YET. BELIEVE I SHOULD ADD AN 'ASYNC' AND 'AWAIT'. NOT SURE WHERE TO PUT THE 'AWAIT'. 
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
