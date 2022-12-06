8e0070f269d133a2afc7febc09ca35bc0a9f1b72
Amy Lipscomb <amylipscomb@macbook-pro.myfiosgateway.com>
2022-12-06 08:47:52 -0500

    added the constraints and validations to product.js.

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,71 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    // * `id`
+    id:{
+      // * Integer.
+      type: DataTypes.INTEGER,
+
+      // * Doesn't allow null values.
+      allowNull: false,
+
+      // * Set as primary key.
+      primaryKey: true,
+
+      // * Uses auto increment.
+        autoIncrement: true,
+    },
+  
+
+  // * `product_name`
+  product_name: {
+      // * String.
+      type: DataTypes.STRING,
+  
+      // * Doesn't allow null values.
+      allowNull: false,
+  },
+
+  // * `price`
+  price: {
+     // * Decimal.
+      type: DataTypes.STRING,
+      
+    // * Doesn't allow null values.
+      allowNull: false,
+    // * Validates that the value is a decimal.
+      validate: {
+        isDecimal: true,
+      }
+  },
+  
+  // * `stock`
+  stock: {
+     // * Integer.
+      type: DataTypes.INTEGER,
+
+     // * Doesn't allow null values.
+      allowNull: false,
+
+    // * Set a default value of `10` & Validates that the value is numeric.
+      validate: {
+        len: [10], // setting the default length as 10
+        isNumeric: true, // validates that the number is numeric
+      }
+  },
+  
+  // * `category_id`
+  category_id: {
+     // * Integer.
+     type: DataTypes.INTEGER,
+
+    // * References the `Category` model's `id`.
+    references: {
+      model: 'category',
+      key: 'id',
+    }
+  }
+  
   },
   {
     sequelize,
