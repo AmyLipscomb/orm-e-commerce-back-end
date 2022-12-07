169430c2ba3ea068c7fffac816322d54d79fa49d
Amy Lipscomb <amylipscomb@amys-mbp.myfiosgateway.com>
2022-12-07 18:33:46 -0500

    revised code in index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,12 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-  Product.belongsTo(Category, {
+ Category.belongsTo(Product, {
     foreignKey: 'category_id',
     onDelete: 'CASCADE',
   });
 // Categories have many Products
-  Category.hasMany(Product, {
+  Product.hasMany(Category, {
     foreignKey: 'category_id',
     onDelete: 'CASCADE',
   });
